872464fa638d6f238ca917b26ae684f5b851eb61
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-04-14 17:56:17 -0300

    a

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,26 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { createUserController } from "../modules/users/useCases/createUser";
+import { listAllUsersController } from "../modules/users/useCases/listAllUsers";
+import { showUserProfileController } from "../modules/users/useCases/showUserProfile";
+import { turnUserAdminController } from "../modules/users/useCases/turnUserAdmin";
 
 const usersRoutes = Router();
 
-const createUserController = new CreateUserController();
+usersRoutes.post("/", (request, response) =>
+  createUserController.handle(request, response)
+);
 
-usersRoutes.post('/', createUserController.handle);
+usersRoutes.patch("/:user_id/admin", (request, response) =>
+  turnUserAdminController.handle(request, response)
+);
+
+usersRoutes.get("/:user_id", (request, response) =>
+  showUserProfileController.handle(request, response)
+);
+
+usersRoutes.get("/", (request, response) =>
+  listAllUsersController.handle(request, response)
+);
 
 export { usersRoutes };
