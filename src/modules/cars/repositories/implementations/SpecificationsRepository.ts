4abf7b0e9d56f509d9b374d6ea3d53e6980c3564
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-04-14 17:51:37 -0300

    ok

---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { Specification } from '../../entities/Specification';
+import {
+  ISpecificationsRepository,
+  ICreateSpecificationDTO,
+} from '../ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private repository: Repository<Specification>;
+
+  constructor() {
+    this.repository = getRepository(Specification);
+  }
+
+  async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({ description, name });
+
+    await this.repository.save(specification);
+  }
+
+  async findByName(name: string): Promise<Specification> {
+    const specification = this.repository.findOne({ name });
+    return specification;
+  }
+
+  async list(): Promise<Specification[]> {
+    const specifications = await this.repository.find();
+    return specifications;
+  }
+}
+
+export { SpecificationsRepository };
